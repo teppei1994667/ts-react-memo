b0aeced21770c95eeb3fe9d8ae8d243994fcff6c
teppei1994667 <teppei.engineer.667@gmail.com>
2023-11-07 23:07:46 +0900

    ControlledTextField内で使用しないpropsをrestPropsで受け取る 変更

--- src/share/ControlledTextField.tsx
+++ src/share/ControlledTextField.tsx
@@ -6,7 +6,7 @@ export type ControlledTextFieldProps = Omit<TextFieldProps, "name"> & {
 };
 
 export const ControlledTextField = (props: ControlledTextFieldProps) => {
-  const { fullWidth, label, variant, name, ...restProps } = props;
+  const { name, ...restProps } = props;
   const { control } = useFormContext();
 
   return (
@@ -19,9 +19,6 @@ export const ControlledTextField = (props: ControlledTextFieldProps) => {
       render={({ field, formState: { errors } }) => (
         <TextField
           {...field}
-          fullWidth={fullWidth}
-          label={label}
-          variant={variant}
           error={!!errors.memo}
           helperText={errors.memo?.message as string}
           {...restProps}
